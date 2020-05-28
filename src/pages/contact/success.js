af9c1c76c82c8923037fe5d91c89f06965f72c95
Charif <ayouni_charif@outlook.fr>
2020-05-28 16:48:51 +0200

    add success page in the contact folder

--- src/pages/contact/success.js
+++ src/pages/contact/success.js
@@ -2,19 +2,26 @@ import React from "react"
 
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
+import backgroundImage from "../../images/contact-bg.jpg";
 
 const Contact = () => (
   <Layout>
     <SEO title="Nous contacter" />
-    <div id="main" className="alt">
-      <section id="one">
-        <div className="inner">
-          <header className="major">
-            <h1>Merci pour votre message</h1>
-          </header>
+    <section id="gtco-reservation" className="bg-fixed bg-white section-padding overlay" style={{backgroundImage: `url(${backgroundImage})`, backgroundRepeat: "no-repeat", backgroundSize: 'cover'}}>
+      <div className="container">
+        <div className="row">
+          <div className="offset-lg-2 col-lg-8 col-md-12">
+            <div className="section-content bg-white p-5 shadow">
+              <div className="heading-section text-center">
+                <h2>
+                  Merci pour votre message
+                </h2>
+              </div>
+            </div>
+          </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   </Layout>
 );
 
