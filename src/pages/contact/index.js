b957f00893ff25766273478de0f86fc27c13a716
Charif <ayouni_charif@outlook.fr>
2020-05-28 16:17:32 +0200

    Contact page

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -2,12 +2,48 @@ import React from "react"
 
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
-import ContactPage from "../../components/contact";
+import backgroundImage from "../../images/contact-bg.jpg";
 
 const Contact = () => (
   <Layout>
     <SEO title="Nous contacter" />
-    <ContactPage />
+    <section id="gtco-reservation" className="bg-fixed bg-white section-padding overlay" style={{backgroundImage: `url(${backgroundImage})`, backgroundRepeat: "no-repeat", backgroundSize: 'cover'}}>
+      <div className="container">
+        <div className="row">
+          <div className="offset-lg-2 col-lg-8 col-md-12">
+            <div className="section-content bg-white p-5 shadow">
+              <div className="heading-section text-center">
+                <h2>
+                  Contactez-nous
+                </h2>
+              </div>
+              <form method="post" name="contact" action="success.js" data-netlify="true">
+                <div className="row">
+                  <div className="col-md-12 form-group">
+                    <input type="text" className="form-control" id="name" name="name" placeholder="Nom"/>
+                  </div>
+                  <div className="col-md-12 form-group">
+                    <input type="text" className="form-control" id="email" name="email" placeholder="Email"/>
+                  </div>
+                  <div className="col-md-12 form-group">
+                    <input type="number" className="form-control" id="phoneNumber" name="phoneNumber" placeholder="Téléphone"/>
+                  </div>
+                  <div className="col-md-12 form-group">
+                    <textarea className="form-control" id="message" name="message" rows="6" placeholder="Votre message ..."/>
+                  </div>
+                  <div className="col-md-12 text-center">
+                    <input type="hidden" name="contact" value="contact" />
+                    <button className="btn btn-primary btn-shadow btn-lg" type="submit" name="submit">
+                      Envoyer
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </Layout>
 );
 
---
+++ src/pages/contact/success.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+
+const Contact = () => (
+  <Layout>
+    <SEO title="Nous contacter" />
+    <div id="main" className="alt">
+      <section id="one">
+        <div className="inner">
+          <header className="major">
+            <h1>Merci pour votre message</h1>
+          </header>
+        </div>
+      </section>
+    </div>
+  </Layout>
+);
+
+export default Contact
