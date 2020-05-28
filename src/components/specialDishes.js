86985a394b0c647c31d196768908760d90e34cbd
Charif <ayouni_charif@outlook.fr>
2020-05-28 14:50:58 +0200

    Special dishes

--- src/components/specialDishes.js
+++ src/components/specialDishes.js
@@ -1,122 +1,81 @@
 import React from "react"
-import dishe1 from "../images/Couscous-agneau.png";
-import dishe2 from "../images/Couscous-poulet-merguez.jpg";
-import dishe3 from "../images/Tajine-poulet-olives.jpg";
 
-const SpecialDishes = () => (
-  <section id="gtco-special-dishes" className="bg-grey section-padding">
-    <div className="container">
-      <div className="section-content">
-        <div className="heading-section text-center">
-          <span className="subheading">
-              Nos
-          </span>
-          <h2>
-            Recettes spéciales
-          </h2>
+const SpecialDishes = (data) => {
+  return (
+      <>
+        {
+          data.align === 'right' ? <Right data={data.data.node.frontmatter} index={data.index} /> : <Left  data={data.data.node.frontmatter} index={data.index} />
+        }
+      </>
+  );
+};
+
+
+const Left = (props) => {
+  return (
+    <div className="row mt-5">
+      <div className="col-lg-5 col-md-6 align-self-center py-5">
+        <h2 className="special-number">0{props.index}.</h2>
+        <div className="dishes-text">
+          <Title title={props.data.title} />
+          <Ingredient ingredients={props.data.ingredients} />
+          <Price price={props.data.price}/>
+          <a href="/" className="btn-primary mt-3">commander</a>
         </div>
-        <div className="row mt-5">
-          <div className="col-lg-5 col-md-6 align-self-center py-5">
-            <h2 className="special-number">01.</h2>
-            <div className="dishes-text">
-              <h3>
-                <span>Couscous </span>
-                <br/>
-                poulet et merguez
-              </h3>
-              <p className="pt-3">
-                <u>Les ingrédients :</u><br/>
-                8 pilons de poulet (fermier de préférence),
-                12 merguez,
-                2 boîtes de concentré de tomates,
-                3 cuillères à soupe d'épices à couscous,
-                1 cuillère à café de harissa,
-                2 cubes de bouillon de boeuf,
-                3 tomates,
-                10 navets,
-                5 carottes,
-                2 courgettes,
-                400 g de pois chiches,
-              </p>
-              <h3 className="special-dishes-price">€15.00</h3>
-              <a href="/" className="btn-primary mt-3">commander</a>
-            </div>
-          </div>
-          <div className="col-lg-5 offset-lg-2 col-md-6 align-self-center mt-4 mt-md-0">
-            <img src={dishe2} className="img-fluid shadow w-100" alt="Couscous poulet et merguez"/>
-          </div>
-        </div>
-
-        <div className="row mt-5">
-          <div className="col-lg-5 col-md-6 align-self-center order-2 order-md-1 mt-4 mt-md-0">
-            <img src={dishe1} className="img-fluid shadow w-100" alt="Couscous algérien à l'agneau"/>
-          </div>
-          <div className="col-lg-5 offset-lg-2 col-md-6 align-self-center order-1 order-md-2 py-5">
-            <h2 className="special-number">02.</h2>
-            <div className="dishes-text">
-              <h3>
-                <span>Couscous</span>
-                <br/>
-                algérien à l'agneau
-              </h3>
-              <p className="pt-3">
-                <u>Les ingrédients :</u><br/>
-                Quelques morceaux de viande g'agneau,
-                2 oignons râpés,
-                2 gousses d'ail râpées,
-                1 tasse de pois-chiches,
-                1 tomate,
-                1 cuillère de concentré de tomates,
-                8 carottes,
-                1 branche de céleri,
-                Sel, poivre,
-                Piment selon votre goût
-
-              </p>
-              <h3 className="special-dishes-price">€12.00</h3>
-              <a href="/" className="btn-primary mt-3">
-                commander
-                <span>
-                  <i className="fa fa-long-arrow-right"></i>
-                </span>
-              </a>
-            </div>
-          </div>
-        </div>
-
-        <div className="row mt-5">
-          <div className="col-lg-5 col-md-6 align-self-center py-5">
-            <h2 className="special-number">03.</h2>
-            <div className="dishes-text">
-              <h3>
-                <span>Tadjine </span>
-                <br/>
-                de poulet aux olives
-              </h3>
-              <p className="pt-3">
-                <u>Les ingrédients :</u><br/>
-                2 cuisses de poulet,
-                1 belle poignée d’olives vertes,
-                1 gros oignon,
-                2 gousses d’ail,
-                2 c. à soupe de jus de citron fraîchement pressé,
-                2 c. à soupe d'épices à base de curry,
-                2 c. à soupe de persil plat haché,
-                3 c. à soupe d'huile d’olive,
-                sel, poivre au goût
-              </p>
-              <h3 className="special-dishes-price">€15.00</h3>
-              <a href="/" className="btn-primary mt-3">commander</a>
-            </div>
-          </div>
-          <div className="col-lg-5 offset-lg-2 col-md-6 align-self-center mt-4 mt-md-0">
-            <img src={dishe3} className="img-fluid shadow w-100" alt="Tadjine de poulet aux olives"/>
-          </div>
-        </div>
-
+      </div>
+      <div className="col-lg-5 offset-lg-2 col-md-6 align-self-center mt-4 mt-md-0">
+        <Image data={props.data} />
       </div>
     </div>
-  </section>
-);
+  )
+};
 
+const Right = (props) => {
+  return(
+    <div className="row mt-5">
+      <div className="col-lg-5 col-md-6 align-self-center order-2 order-md-1 mt-4 mt-md-0">
+        <Image data={props.data} />
+      </div>
+      <div className="col-lg-5 offset-lg-2 col-md-6 align-self-center order-1 order-md-2 py-5">
+        <h2 className="special-number">0{props.index}.</h2>
+        <div className="dishes-text">
+          <Title title={props.data.title} />
+          <Ingredient ingredients={props.data.ingredients} />
+          <Price price={props.data.price}/>
+          <a href="/" className="btn-primary mt-3">commander</a>
+        </div>
+      </div>
+    </div>
+)
+};
+
+const Image = (props) => {
+  return <img src={props.data.image} className="img-fluid shadow w-100" alt={props.data.title} />
+};
+const Title = (props) => {
+  let title = props.title;
+  const first_word = (title.split(' '))[0];
+  const rest_title = title.substr(first_word.length + 1);
+
+  return(
+    <h3>
+      <span>{first_word}</span>
+      <br/>
+      {rest_title}
+    </h3>
+  )
+};
+const Ingredient = (props) => {
+  return(
+    <p className="pt-3">
+      <u>Les ingrédients :</u><br/>
+      {props.ingredients}
+    </p>
+  )
+};
+const Price = (props) => {
+  return <h3 className="special-dishes-price">€ {Number.parseFloat(props.price).toFixed(2)}</h3>
+}
+
+SpecialDishes.propTypes = {};
 export default SpecialDishes
--- src/pages/dishes/index.js
+++ src/pages/dishes/index.js
@@ -2,13 +2,48 @@ import React from "react"
 
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
-import SpecialDishesPage from "../../components/specialDishes";
+import Dishes from "../../components/specialDishes";
+import {graphql} from "gatsby";
 
-const SpecialDishes = () => (
-  <Layout>
-    <SEO title="Nos Recettes spéciales" />
-    <SpecialDishesPage />
-  </Layout>
-);
+export const query = graphql`{
+    allMarkdownRemark(filter: {frontmatter: {categorie: {eq: "special"}}}) {
+      edges {
+        node {
+          frontmatter {
+            title
+            price
+            ingredients
+            image
+          }
+        }
+      }
+    }
+}`;
+const SpecialDishes = ({data}) => {
+  return(
+    <Layout>
+      <SEO title="Nos Recettes spéciales" />
+      <section id="gtco-special-dishes" className="bg-grey section-padding">
+        <div className="container">
+          <div className="section-content">
+            <div className="heading-section text-center">
+              <span className="subheading">
+                  Nos
+              </span>
+              <h2>
+                Recettes spéciales
+              </h2>
+            </div>
+            {
+              data.allMarkdownRemark.edges.map((dishe, index) => {
+                return <Dishes align={ index % 2 ? 'right' : 'left' } data={dishe} index={index+1} />
+              })
+            }
+          </div>
+        </div>
+      </section>
+    </Layout>
+  )
+};
 
 export default SpecialDishes
